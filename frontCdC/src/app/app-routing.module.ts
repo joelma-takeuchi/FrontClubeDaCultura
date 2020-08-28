d51f00921a03b1eb1f803772c6521444e466d5a7
Mauricio <mauriciogrs95@gmail.com>
2020-08-28 13:25:53 -0300

    Merge da branch rotas

--- frontCdC/src/app/app-routing.module.ts
+++ frontCdC/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+import { PlanosComponent } from './planos/planos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'sobre', component: SobreComponent},
+  {path: 'contato', component: ContatoComponent },
+  {path: 'planos', component: PlanosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
