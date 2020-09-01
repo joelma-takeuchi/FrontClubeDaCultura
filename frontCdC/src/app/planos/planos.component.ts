b1b56fd64c6a95de7bb6471f79771b3b77d9086a
WILLIANS CARRION <willianscarrion@gmail.com>
2020-09-01 16:28:59 -0300

    inclusão do método GET no component planos

--- frontCdC/src/app/planos/planos.component.ts
+++ frontCdC/src/app/planos/planos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from '../model/Produto';
+import { Categoria } from '../model/Categoria';
+import { ProdutoService } from '../service/produto.service';
+import { CategoriaService } from '../service/categoria.service';
 
 @Component({
   selector: 'app-planos',
@@ -7,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanosComponent implements OnInit {
 
-  constructor() { }
+  produto: Produto = new Produto()
+  listaProdutos: Produto[]
+
+  categoria: Categoria = new Categoria()
+  listaCategorias: Categoria[]
+  idCategoria: number
+  
+  constructor(
+    private produtoService: ProdutoService,
+    private categoriaService: CategoriaService
+  ) { }
 
   ngOnInit(){
     window.scroll(0,0)
+
+    this.findAllProdutos()
+    this.findAllCategorias()
   }
 
+  findAllProdutos() {
+    this.produtoService.getAllProdutos().subscribe((resp: Produto[]) => {this.listaProdutos = resp})
+  }
+
+  findAllCategorias() {
+    this.categoriaService.getAllCategorias().subscribe((resp: Categoria[]) => {this.listaCategorias = resp})
+  }
+
+  findByIdCategoria() {
+    this.categoriaService.getByIdCategoria(this.idCategoria).subscribe((resp: Categoria) => {this.categoria = resp;})
+  }
 }
--- frontCdC/src/app/service/categoria.service.ts
+++ frontCdC/src/app/service/categoria.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
+  }
+
+  getAllCategorias() {
+    return this.http.get('http://localhost:8080/categoria', this.token)
+  }
+
+  getByIdCategoria(id: number) {
+    return this.http.get(`http://localhost:8080/categoria/${id}`, this.token)
+  }
 }
